41fd52bc4603e49031b8d2a983b12ebb81105e66
MykolaBerkosha <mberkosa@gmail.com>
2022-02-16 10:14:01 +0200

    Update fields.

--- faq/__init__.py
+++ faq/__init__.py
@@ -2,6 +2,16 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
+def setup_settings(settings, is_prod, **kwargs):
+
+    settings['INSTALLED_APPS'] += [
+        app for app in [
+            'faq',
+            'ordered_model'
+        ] if app not in settings['INSTALLED_APPS']
+    ]
+
+
 class FAQConfig(AppConfig):
     name = 'faq'
     verbose_name = _("FAQ")
